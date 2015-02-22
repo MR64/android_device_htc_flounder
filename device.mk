5719a855cbcf05a1779a3377f8d2c2ea429cfd38
Joshua <joshuabg@users.noreply.github.com>
2015-02-22 15:20:33 -0500

    Additional flags for adb

--- device.mk
+++ device.mk
@@ -196,8 +196,9 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp \
+    persist.sys.usb.config=mtp,adb \
     ro.adb.secure=0
+    ro.secure=0
 
 # for off charging mode
 PRODUCT_PACKAGES += \
